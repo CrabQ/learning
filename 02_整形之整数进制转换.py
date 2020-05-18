a46a50e8a68018ed89160c7e41aa4f4303b0d7f7
CrabQ <18819425701@163.com>
2020-05-18 17:01:31 +0800

    学习

---
+++ 02_整形之整数进制转换.py
@@ -0,0 +1,14 @@
+# 转换为二进制
+print(bin(10))
+print(int('0b1010', 2))
+
+# 转换为八进制
+print(oct(10))
+print(int('0o12', 8))
+
+# 转换为十六进制
+print(hex(10))
+print(int('0xa', 16))
+
+a = list({'1':1,'2':2})
+print(a)
